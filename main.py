0162c9eb421962f1a65392e23b9f75d8e03e503b
Carrie West <carrie105violet@gmail.com>
2021-07-23 14:43:27 -0400

    Built a mechanism to clear the .cache file, for more accurate and varied testing.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import spotipy
 import openpyxl
 from spotipy.oauth2 import SpotifyOAuth
 
+
 scope = "user-library-read"
 
 client_id = os.environ['CLIENT_ID']
 client_secret = os.environ['CLIENT_SECRET']
 redirect_url = "https://replit.com/@CarrieViolet"  # Would be replaced with the host website
 
+
 sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_url))
+    auth_manager=SpotifyOAuth(
+        scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_url, open_browser=True, ))
 
 song_data = pd.DataFrame([], columns=["Track Name", "Album", "Artist"])
 
@@ -33,10 +36,10 @@ while playlists:
                 song_count = song_count + 1
                 if song_data.empty:
                     song_data.loc[0] = [song['track']['name'], song['track']['album']['name'],
-                                song['track']['artists'][0]['name']]
+                                        song['track']['artists'][0]['name']]
                 else:
-                    song_data.loc[song_data.index[-1]+1] = [song['track']['name'], song['track']['album']['name'],
-                                song['track']['artists'][0]['name']]
+                    song_data.loc[song_data.index[-1] + 1] = [song['track']['name'], song['track']['album']['name'],
+                                                              song['track']['artists'][0]['name']]
             if song_count == song_max:
                 songs = None
     if playlists['next']:
@@ -55,17 +58,16 @@ while liked_songs:
             song_data.loc[0] = [song['track']['name'], song['track']['album']['name'],
                                 song['track']['artists'][0]['name']]
         else:
-            song_data.loc[song_data.index[-1]+1] = [song['track']['name'], song['track']['album']['name'],
-                                song['track']['artists'][0]['name']]
+            song_data.loc[song_data.index[-1] + 1] = [song['track']['name'], song['track']['album']['name'],
+                                                      song['track']['artists'][0]['name']]
     if song_count == 50:
         liked_songs = sp.current_user_saved_tracks(limit=50, offset=song_count)
     else:
         liked_songs = None
 
-
 album_count = 0
-saved_albums = sp.current_user_saved_albums(limit=50,offset=album_count)
-album_data = pd.DataFrame([],columns=["Album Name", "Artist"])
+saved_albums = sp.current_user_saved_albums(limit=50, offset=album_count)
+album_data = pd.DataFrame([], columns=["Album Name", "Artist"])
 while saved_albums:
 
     for album in saved_albums['items']:
@@ -73,7 +75,7 @@ while saved_albums:
         if album_data.empty:
             album_data.loc[0] = [album['album']['name'], album['album']['artists'][0]['name']]
         else:
-            album_data.loc[album_data.index[-1]+1] = [album['album']['name'], album['album']['artists'][0]['name']]
+            album_data.loc[album_data.index[-1] + 1] = [album['album']['name'], album['album']['artists'][0]['name']]
     if album_count == 50:
         saved_albums = sp.current_user_saved_albums(limit=50, offset=album_count)
         album_count = 0
@@ -83,3 +85,4 @@ print("Done!")
 with pd.ExcelWriter('output.xlsx', engine='openpyxl', mode='w') as writer:
     song_data.to_excel(writer, sheet_name='Liked Songs and Playlists')
     album_data.to_excel(writer, sheet_name='Saved Albums')
+os.remove('.cache')
